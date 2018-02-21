a6a8ccbe2fd646bb68053effa7bc2b861d34e227
Mggeroff <roman.mager@outlook.com>
2018-02-22 00:15:52 +0300

    All tests pass

--- src/index.js
+++ src/index.js
@@ -4,4 +4,17 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  let loveTriangle = 0;
+  for (let i = 0; i < preferences.length; i++) {
+    let one, two, three;
+
+    one = preferences[i];
+    two = preferences[one-1];
+    three = preferences[two-1];
+    if ((i + 1) === three) {
+      loveTriangle++;
+    }
+
+  }
+  return Math.floor(loveTriangle/3);
 };
